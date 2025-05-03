5aa942823ff9857f2cd3358afc99ae46cabcba41
Luca Sartore <lucasar.com8@gmail.com>
2025-05-03 17:13:25 +0200

    Remove deprecated use ov validator

--- src/robot_arm/src/internal_types/urdf_model.py
+++ src/robot_arm/src/internal_types/urdf_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, root_validator
+from pydantic import BaseModel, Field, model_validator
 from typing import List
 
 
@@ -10,12 +10,14 @@ class Origin(BaseModel):
     rpy: List[float] = Field(alias='@rpy')
     xyz: List[float] = Field(alias='@xyz')
 
-    @root_validator(pre=True)
+    @model_validator(mode='before')
+    @classmethod
     def split_space_separated_values(cls, values):
         for key in ['@rpy', '@xyz']:
             if key in values:
                 values[key] = [float(v) for v in values[key].split()]
         return values
+
 
 
 class Joint(BaseModel):
--- src/robot_arm/src/main.py
+++ src/robot_arm/src/main.py
@@ -3,4 +3,4 @@
 from constants.path import URDF_FILE_PATH
 from data_loader.load_urdf import load_urdf
 print("hello world from ros!", URDF_FILE_PATH)
-load_urdf()
+print(load_urdf())
