7f3c447898851d0acde78ebdbff742a1f065a007
Luca Sartore <lucasar.com8@gmail.com>
2025-05-04 17:40:38 +0200

    Add friction and dumpening to urdf model

--- src/robot_arm/src/internal_types/urdf_model.py
+++ src/robot_arm/src/internal_types/urdf_model.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field, model_validator
-from typing import List, Literal
+from typing import List, Literal, Optional
 
 
 class LinkRef(BaseModel):
@@ -19,6 +19,28 @@ class Origin(BaseModel):
         return values
 
 
+class Axis(BaseModel):
+    xyz: List[float] = Field(alias='@xyz')
+
+    @model_validator(mode='before')
+    @classmethod
+    def split_xyz(cls, values):
+        if '@xyz' in values:
+            values['@xyz'] = [float(v) for v in values['@xyz'].split()]
+        return values
+
+
+class Limit(BaseModel):
+    effort: float = Field(alias='@effort')
+    lower: float = Field(alias='@lower')
+    upper: float = Field(alias='@upper')
+    velocity: float = Field(alias='@velocity')
+
+
+class Dynamics(BaseModel):
+    damping: float = Field(alias='@damping')
+    friction: float = Field(alias='@friction')
+
 
 class Joint(BaseModel):
     name: str = Field(alias='@name')
@@ -26,3 +48,5 @@ class Joint(BaseModel):
     parent: LinkRef
     child: LinkRef
     origin: Origin
+    limit: Optional[Limit] = None
+    dynamics: Optional[Dynamics] = None
