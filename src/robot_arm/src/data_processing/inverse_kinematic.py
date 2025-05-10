7b96f55663b08c99bd2b567535008cd9aeecff24
Luca Sartore <lucasar.com8@gmail.com>
2025-05-10 17:25:14 +0200

    Add desired pose in task null space

--- src/robot_arm/src/data_processing/inverse_kinematic.py
+++ src/robot_arm/src/data_processing/inverse_kinematic.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import List, Union
 from data_processing.direct_kinematics import build_analytical_jacobian, get_progressive_transformation_matrixes, build_jacobian, get_components_from_transf_matrix
 import numpy as np
@@ -19,6 +20,10 @@ def inverse_kinematic(
     gamma_constant: float = 0.5,
     epsilon_constant: float = 1e-5,
     max_steps = 100,
+    use_postural_task = False,
+    desired_pose: Union[List[float], None] = None,
+    desired_pose_relative_weights: Union[List[float], None] = None,
+    weight_postural_task: float = 1e-4,
     visualize = False
 ) -> List[float]:
     """
@@ -39,6 +44,18 @@ def inverse_kinematic(
     else:
         assert len(initial_guess) == num_joints, f"The provided initial guess has len = {len(initial_guess)}, expect {num_joints} instead"
 
+    if desired_pose is None:
+        desired_pose = [0.0]*num_joints
+    else:
+        assert len(desired_pose) == num_joints, f"The desired pose has len = {len(desired_pose)}, expect {num_joints} instead"
+    desired_pose = np.array(desired_pose, dtype = np.float64) #type: ignore
+
+    if desired_pose_relative_weights is None:
+        desired_pose_relative_weights = [1.0]*num_joints
+    else:
+        assert len(desired_pose_relative_weights) == num_joints, f"The desired pose relative_weights has len = {len(desired_pose_relative_weights)}, expect {num_joints} instead"
+    desired_pose_relative_weights = np.array(desired_pose_relative_weights, dtype = np.float64) #type: ignore
+
     desired_pos_and_or = np.array([
         x if x is not None else np.nan
         for x in desired_position + desired_orientation
@@ -57,23 +74,44 @@ def inverse_kinematic(
 
     
     mm = np.matmul
+    def mm_chain(*mats) -> Array:
+        mat_array = [m for m in mats]
+        return reduce(mm, mat_array)
 
-    size = build_jacobian(joints, list(current_guess)).shape[1]
-    lambda_i = lambda_constant * np.identity(size)
+
+    identity = np.identity(num_joints)
 
     # print(f"Desired position: {desired_pos_and_or}")
     for _ in range(max_steps):
         pos = get_current_position(current_guess)
-        error = pos - desired_pos_and_or #type: ignore
-
-        # remove the NaN components
-        error = error[np.isnan(error) == False]
 
         j = build_analytical_jacobian(joints, list(current_guess))
         # remove the components that are not part of our task space variables
         j = j[np.isnan(desired_pos_and_or) == False] #type: ignore
 
-        delta_q = - mm(np.linalg.inv(mm(j.T,j) + lambda_i), mm(j.T, error)) #type: ignore 
+        error = pos - desired_pos_and_or #type: ignore
+        # remove the NaN components
+        error = error[np.isnan(error) == False]
+
+        if use_postural_task:
+            error = np.hstack((error, -weight_postural_task * desired_pose_relative_weights * (desired_pose - current_guess))) #type: ignore
+            j_plus_identity = np.vstack((j, weight_postural_task * identity)) #type: ignore
+
+            delta_q = -mm_chain(
+                np.linalg.inv(mm(j.T,j) + weight_postural_task**2*identity), #type: ignore
+                j_plus_identity.T,
+                error
+            )
+        else:
+
+            # print(np.linalg.inv(mm(j.T,j) + weight_postural_task**2*identity).shape)
+            # print(j.T.shape)
+            # print(error.shape)
+            delta_q = -mm_chain(
+                np.linalg.inv(mm(j.T,j) + lambda_constant * identity), #type: ignore
+                j.T,
+                error
+            )
 
         error_abs = old_error = np.sum(error**2)**0.5
         # print(f"Current position: {pos}")
@@ -121,8 +159,37 @@ def test_inverse_kinematics():
 
     initial_guess: list[float] = [2, 1, 1, 0, 0]
     # position of the end effector (x,y,z)
+    position: List[Union[float, None]] = [4,4,1]
+    # orientation of the end effector (roll, pitch, yaw.) orient with 30 degrees angle ad the end like specifications
+    orientation: List[Union[float, None]] = [None, None, None]
+    # orientation: List[Union[float, None]] = [None, math.radians(-30), None]
+
+    inverse_kinematic(joints, position, orientation, initial_guess, visualize=False)
+
+
+def test_inverse_kinematics_with_postural():
+    joints = load_urdf()
+
+    initial_guess: list[float] = [2, 1, 1, 0, 0]
+    # position of the end effector (x,y,z)
     position: List[Union[float, None]] = [1,2,1]
     # orientation of the end effector (roll, pitch, yaw.) orient with 30 degrees angle ad the end like specifications
-    orientation: List[Union[float, None]] = [None, math.radians(-30), None]
+    orientation: List[Union[float, None]] = [None, None, None]
+    # orientation: List[Union[float, None]] = [None, math.radians(-30), None]
 
-    inverse_kinematic(joints, position, orientation, initial_guess, visualize=True)
+    # desired_pose: List[float] = [0,0,0,0,0]
+    desired_pose: List[float] = [0,0,0,0,0]
+    # I only care about the position of the second joint, all of the others can do what they prefer
+    desired_pose_relative_weights: List[float] = [0,1,0,0,0]
+
+    inverse_kinematic(
+        joints,
+        position,
+        orientation,
+        initial_guess,
+        visualize=True,
+        use_postural_task=True,
+        desired_pose=desired_pose,
+        weight_postural_task=0.6,
+        desired_pose_relative_weights=desired_pose_relative_weights
+    )
--- src/robot_arm/src/main.py
+++ src/robot_arm/src/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
-from data_processing.inverse_kinematic import test_inverse_kinematics
+from data_processing.inverse_kinematic import test_inverse_kinematics, test_inverse_kinematics_with_postural
 
 test_inverse_kinematics()
+test_inverse_kinematics_with_postural()
