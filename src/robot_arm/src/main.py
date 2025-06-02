58fe530a9ef6dba50556d1ae5bffe09a0a3d14ed
Luca Sartore <lucasar.com8@gmail.com>
2025-06-02 08:36:12 +0200

    Add cli

--- src/robot_arm/src/main.py
+++ src/robot_arm/src/main.py
@@ -1,10 +1,48 @@
 #!/usr/bin/env python3
+import argparse
+
 from data_processing.full_simulation import run_full_simulation
-from data_processing.inverse_kinematic import test_inverse_kinematics_with_postural, test_inverse_kinematics
-from data_processing.dynamics import test_dynamics, test_dynamics_with_initial_velocity
+from data_processing.inverse_kinematic import (
+    test_inverse_kinematics_with_postural,
+    test_inverse_kinematics,
+)
+from data_processing.dynamics import (
+    test_dynamics,
+    test_dynamics_with_initial_velocity,
+)
 
-test_dynamics_with_initial_velocity()
-test_dynamics()
-test_inverse_kinematics_with_postural()
-run_full_simulation()
 
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run different robotics simulation tests."
+    )
+    parser.add_argument(
+        "mode",
+        choices=[
+            "run_full_simulation",
+            "test_inverse_kinematics_with_postural",
+            "test_inverse_kinematics",
+            "test_dynamics",
+            "test_dynamics_with_initial_velocity",
+        ],
+        nargs="?",
+        default="run_full_simulation",
+        help="Which function to run (default: run_full_simulation)",
+    )
+
+    args = parser.parse_args()
+
+    if args.mode == "run_full_simulation":
+        run_full_simulation()
+    elif args.mode == "test_inverse_kinematics_with_postural":
+        test_inverse_kinematics_with_postural()
+    elif args.mode == "test_inverse_kinematics":
+        test_inverse_kinematics()
+    elif args.mode == "test_dynamics":
+        test_dynamics()
+    elif args.mode == "test_dynamics_with_initial_velocity":
+        test_dynamics_with_initial_velocity()
+
+
+if __name__ == "__main__":
+    main()
